261d650d58d4f3be1c508ee26a89d87d81449215
unknown <rohithasatya314@gmail.com>
2021-06-27 23:59:45 +0530

    MODIFIED:Display Board

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -1,32 +1,84 @@
 package com.bridgelabz.tictactoe;
-import java.util.Scanner;
-public class TicTacToe {
-	public static void main(String[] args) {
-		System.out.println("Welcome to tic tac toe");
-	}
-	public static char[] createBoard() {
-		char[] board =new char[10];
-		for(int i = 1; i <board.length; i++) {
-						board[i]= ' ';
-					}
-				return board;
-			}
-			char[] board = createBoard();
-			 static char chooseOption() {
-					char computersSymbol=0;
-					Scanner scanner =new Scanner(System.in);
-					System.out.println("Give an option 'o' or 'x':");
-					char option = scanner.next().charAt(0);
-					scanner.close();
-					if (option == 'o' ) {
-						computersSymbol='x';	
-					}else if (option == 'x' ) {
-						computersSymbol ='o';
-					}else {
-						System.out.println("invalid option:");
-					}
-				System.out.println("Players option:"+ option+ "computers option is:"+computersSymbol);
-					return option;
-				}
-			}
 
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static char board[] = new char[10];
+    public static char[] printMatrix() {
+        board = new char[10];
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
+        return board;
+    }
+
+    public static char getInput(Scanner userInput) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Choose between x and 0 ");
+        return userInput.next().toUpperCase().charAt(0);
+    }
+
+    public static void displayBoard() {
+        int k = 1;
+        for (int i = 1; i < 3; i++) {
+            for (int j = 1; j < 3; j++) {
+                //System.out.print(k);
+                System.out.print(board[k]);
+                k++;
+                if (j < 3) {
+                    System.out.print(" | ");
+                }
+
+            }
+            System.out.println();
+            System.out.println("____");
+
+
+        }
+        System.out.println("  |   |   ");
+    }
+
+    public static int getUserMove(char[] board, Scanner userInput) {
+        Integer[] validCells = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        while (true) {
+            System.out.println("What is your next move");
+            int index = userInput.nextInt();
+            if (Arrays.asList(validCells).contains(index) && isSpaceFree(board, index)) {
+                return index;
+            }
+            else
+            {
+                System.out.println("Position is already filled");
+            }
+        }
+    }
+    private static void checkFreePosition(int userMove)
+    {
+        Scanner scan=new Scanner(System.in);
+        board[userMove]=getInput(scan);
+        displayBoard();
+
+    }
+    private static boolean isSpaceFree(char[] board, int index) {
+        return board[index] == ' ';
+    }
+
+    public static void main(String[] args) {
+        char computer, user;
+        System.out.println("Welcome");
+        char board[] = printMatrix();
+        Scanner userInput = new Scanner(System.in);
+        char userLetter = getInput(userInput);
+        char computerLetter = (userLetter == 'X') ? '0' : 'X';
+        System.out.println("computerLetter" + computerLetter);
+        displayBoard();
+        //UC4
+        int userMove = getUserMove(board, userInput);
+        userLetter = getInput(userInput);
+        board[userMove] = userLetter;
+        displayBoard();
+        //UC5
+        userMove=getUserMove(board,userInput);
+        checkFreePosition(userMove);
+    }
+}
